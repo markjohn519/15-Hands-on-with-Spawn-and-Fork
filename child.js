6685cacd510a0b640987adb4519174c0aeda93c7
markjohn519 <markjohnmonton@gmail.com>
2023-01-10 10:16:56 +0800

    working Spawn() child & parent

--- child.js
+++ child.js
@@ -1,25 +1,17 @@
 import crypto from 'node:crypto'
 
-process.stdin.resume()
-
-let parentPublicKey
-let parentFile
-
 process.stdin.on('data', (data) => {
   try {
     const { publicKey, fileContent } = JSON.parse(data)
-    parentPublicKey = publicKey
-    parentFile = fileContent
+
+    const encryptedContent = crypto.publicEncrypt(publicKey, Buffer.from(fileContent, 'base64'))
+
+    process.stdout.write(encryptedContent)
   } catch (error) {
     console.error(error.message)
   }
 })
 
 process.stdin.on('end', () => {
-  const buffer = Buffer.from(parentFile)
-  const encrypted = crypto.publicEncrypt(parentPublicKey, buffer)
-
-  console.log(parentFile)
-  process.stdout.write(encrypted)
   process.stdout.end()
 })
--- parent.js
+++ parent.js
@@ -1,6 +1,6 @@
 import fs from 'node:fs'
 import { spawn } from 'child_process'
-import { createVerify } from 'node:crypto'
+import { privateDecrypt } from 'node:crypto'
 
 import * as dotenv from 'dotenv'
 
@@ -8,32 +8,29 @@ import generateKeys from './generateKeyPairSync.js'
 
 const { SECRET } = dotenv.config().parsed
 const { publicKey, privateKey } = generateKeys(SECRET)
-const fileContent = fs.readFileSync('memoryMonitor.js', 'utf8')
+const fileContent = fs.readFileSync('text.txt', 'utf8')
 
 const child = spawn('node', ['child.js'])
 
 child.stdin.write(JSON.stringify({ publicKey, fileContent }))
 child.stdin.end()
 
-let encryptedFile = ''
+let encryptedFile
 child.stdout.on('data', (data) => {
-  console.log(data.toString())
-  encryptedFile += data
+  encryptedFile = data
 })
 
 child.stdout.on('end', () => {
-  console.log(encryptedFile)
-  // Verify the encrypted file is actually the original file
-  const verifier = createVerify('sha256')
-  verifier.update(fileContent)
-  const result = verifier.verify(publicKey, encryptedFile, 'hex')
-  if (result) {
-    console.log('The encrypted file is valid.')
+  const decryptedData = privateDecrypt(
+    { key: privateKey, passphrase: SECRET },
+    encryptedFile,
+  )
+  if (decryptedData.toString('base64') === fileContent) {
+    console.log(`The encrypted file content '${decryptedData.toString('base64')}' is correct.`)
   } else {
-    console.error('The encrypted file is not valid.')
+    console.error('The encrypted file content is not correct.')
   }
 
-  // Measure time and memory usage
   console.timeEnd('execution-time')
   console.log(`Memory usage: ${Math.round(process.memoryUsage().rss / 1024 / 1024)} MB`)
 })
